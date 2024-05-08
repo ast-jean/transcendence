bf2eb9ba44f3bf39ec8fd5d8bb62272ebd4cbc09
ast-jean <adam.stjean.lejeune@gmail.com>
2024-05-08 18:55:58 +0000

    added /pdfs and communication with client -> django channels

--- django_trans/main/static/main/scripts/chat.js
+++ django_trans/main/static/main/scripts/chat.js
@@ -1,11 +1,38 @@
+import { socket } from './socket.js'
+
+var input = document.getElementById('chat-input');
+
+//Listening the button click
+document.addEventListener('DOMContentLoaded', function() {
+    const sendButton = document.getElementById('chat-btn');
+    const chatInput = document.getElementById('chat-input');
+
+    sendButton.addEventListener('click', sendMessage);
+    chatInput.addEventListener('keydown', function(event){
+        if (event.key == 'Enter') {
+            event.preventDefault();
+            sendMessage();
+        }
+    });
+});
+
+//Receiving message from server
+socket.addEventListener("message", (event) => {
+    var li = document.createElement('li');
+    li.textContent = event.id + ": " + event.message;
+    // console.log('Message from server" ', event.data);
+});
+
 
 function sendMessage() {
-    var input = document.getElementById('chat-input');
     var message = input.value.trim();
+    var name = "Me";
     if (message !== "") {
+        console.log("sendMsg(): ",socket.readyState);
         var chatBox = document.getElementById('chat-messages');
         var li = document.createElement('li');
-        li.textContent = message;
+        li.textContent = name + ": " + message;
+        socket.send(JSON.stringify({ message })); //Sending to gameserv/consumers.py 
         chatBox.appendChild(li);
         input.value = "";
         chatBox.scrollTop = chatBox.scrollHeight; 
