6389cbf03b61af0fad8a268b10a716d5c76e6fb8
ast-jean <adam.stjean.lejeune@gmail.com>
2024-06-21 19:02:45 +0000

    muting include ELK yml causing issue to main + adding 2x2 and 1x1 button and functionnalities for truck game

--- django_trans/gameserv/consumers_truck.py
+++ django_trans/gameserv/consumers_truck.py
@@ -1,10 +1,12 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import uuid
 import json
+import random
 
 class GameConsumer_truck(AsyncWebsocketConsumer):
 	connected_clients = []
 	rooms = []
+	existing_room_ids = []
 
 	async def find_room(self, room_id):
 		for room in self.rooms:
@@ -49,17 +51,77 @@ class GameConsumer_truck(AsyncWebsocketConsumer):
 				await self.broadcast_move(text_data_json)
 			if text_data_json["cmd"] == "roomSearch":
 				await self.searchRoom(text_data_json)
-			if text_data_json["cmd"] == "roomCreate":
-				await self.searchCreate(text_data_json)
-
+			if text_data_json["cmd"] == "roomCreate2":
+				await self.createRoom2()
+			if text_data_json["cmd"] == "roomCreate4":
+				await self.createRoom4()
 	# async def broadcast_chat(self, data):
 	# 	for client in self.connected_clients:
 	# 		if client.ident != self.ident:
 	# 			await client.send(json.dumps(data))
 
-	async def searchCreate(self, data):
-		print("Creating new room")
 
+	def generate_room_id(self):
+		print("id generator")
+		lower_bound=1000
+		upper_bound=9999
+		room_id = random.randint(lower_bound, upper_bound)
+		while room_id in self.existing_room_ids:
+			room_id = random.randint(lower_bound, upper_bound)	
+		self.existing_room_ids.append(room_id)
+		return room_id
+
+
+
+	async def create_room(self, num_players):
+		print(f"Creating new room for {num_players} players.")
+		try:
+			room_id = self.generate_room_id()
+			data = {
+				"cmd": "roomCreated",
+				"roomId": room_id,
+				"numPlayers": num_players  # Include the number of players in the data if needed
+			}
+			# Assuming 'connected_clients' is a list of client objects with an 'ident' attribute
+			for client in self.connected_clients:
+				if client.ident == self.ident:
+					await self.find_and_add_client(room_id, self.ident)
+					await client.send(json.dumps(data))
+					break  # Stop the loop once the initiating client is processed
+		except Exception as e:
+			print(f"Error creating room: {str(e)}")
+
+
+
+	async def createRoom2(self):
+		print("Creating new room")
+		try:
+			room_Id = self.generate_room_id()
+			data = {
+				"cmd": "roomCreated",
+				"roomId": room_Id
+			}
+			for client in self.connected_clients:
+				if client.ident == self.ident:
+					await self.find_and_add_client(room_Id, self.ident)
+					await client.send(json.dumps(data))
+		except Exception as e:
+			print(f"Error creating room: {str(e)}")
+
+	async def createRoom4(self):
+		print("Creating new room")
+		try:
+			room_Id = self.generate_room_id()
+			data = {
+				"cmd": "roomCreated",
+				"roomId": room_Id
+			}
+			for client in self.connected_clients:
+				if client.ident == self.ident:
+					await self.find_and_add_client(room_Id, self.ident)
+					await client.send(json.dumps(data))
+		except Exception as e:
+			print(f"Error creating room: {str(e)}")
 
 	async def searchRoom(self, data):
 		print(f"Client {self.ident} is searching for room {data['roomId']}")
@@ -75,7 +137,6 @@ class GameConsumer_truck(AsyncWebsocketConsumer):
 			for client in self.connected_clients:
 				if client.ident == self.ident:
 					await client.send(json.dumps(data))
-
 
 	async def broadcast_move(self, data):
 		for client in self.connected_clients:
