64136543d4f5bc52e4fc5b7c1225dfa34339a216
slavoie <slavoie>
2024-09-06 17:38:38 +0000

    fix score local + end game

--- django_trans/main/static/main/scripts/pong.js
+++ django_trans/main/static/main/scripts/pong.js
@@ -148,7 +148,6 @@ export function startCountdown() {
         if (countdown === 0) {
             clearInterval(interval);
             document.body.removeChild(countdownContainer);
-            // console.log("ball start");
             isGameOver = false;
             ballSpeedX = INITIAL_BALL_SPEED_X;
             ballSpeedY = INITIAL_BALL_SPEED_Y;
@@ -189,7 +188,7 @@ let aiPlayer = null;
 
 
 function localPlay() {
-    console.log("Starting local play");
+    // console.log("Starting local play");
 
     players.forEach(player => scene.remove(player.mesh));
     players = [];
@@ -204,7 +203,7 @@ function localPlay() {
 }
 
 function playAI() {
-    console.log("Starting play with AI");
+    // console.log("Starting play with AI");
 
     players.forEach(player => scene.remove(player.mesh));
     players = [];
@@ -277,11 +276,11 @@ function waitForRoomId() {
 
 //click PlayOnline() -> show Layer2Btns -> Click game mode -> HideLayer2 & waitGameFull -> Start Countdown & Start Game
 async function playOnline(maxPlayers) {
-    console.log("Starting online play");
+    // console.log("Starting online play");
 
     if (socketState.isSocketReady) {
         // Send game mode and wait for joinRoom() from server
-        console.log("Connected to server socket")
+        // console.log("Connected to server socket")
         players.forEach(player => scene.remove(player.mesh));
         players = [];
         var cmd = "roomCreate" + maxPlayers;
@@ -350,9 +349,27 @@ export function updatePlayerVisualization() {
 
 
 
+let player1Score = 0;
+let player2Score = 0;
+const maxScore = 5;
+
 function updateScore(player) {
     let team = player === 1 ? "team1" : "team2";
-    
+
+    if (player === 1) {
+        if (player1Score < maxScore) {
+            player1Score++;
+        }
+    } else if (player === 2) {
+        if (player2Score < maxScore) {
+            player2Score++;
+        }
+    }
+
+    updateScoreDisplay();  // Met à jour l'affichage des scores
+    checkEndGame();        // Vérifie si la partie doit se terminer
+
+    // Envoie les scores au serveur si on est en ligne
     if (socketState.socket && socketState.isSocketReady) {
         let cmd = "score";
         let roomId = getRoomId();
@@ -361,21 +378,26 @@ function updateScore(player) {
 }
 
 
-// function updateScoreDisplay() {
-//     player1ScoreElement.innerHTML = getScoreHTML(player1Score, '🟢', maxScore);
-//     player2ScoreElement.innerHTML = getScoreHTML(player2Score, '🔵', maxScore);
-// }
 
-// function getScoreHTML(score, symbol, maxScore) {
-//     let scoreHTML = '';
-//     for (let i = 0; i < score; i++) {
-//         scoreHTML += symbol;
-//     }
-//     for (let i = score; i < maxScore; i++) {
-//         scoreHTML += '⚪';
-//     }
-//     return scoreHTML;
-// }
+const player1ScoreElement = document.getElementById('player1Score');
+const player2ScoreElement = document.getElementById('player2Score');
+
+function updateScoreDisplay() {
+    player1ScoreElement.innerHTML = getScoreHTML(player1Score, '🟢', maxScore);
+    player2ScoreElement.innerHTML = getScoreHTML(player2Score, '🔵', maxScore);
+}
+
+function getScoreHTML(score, symbol, maxScore) {
+    let scoreHTML = '';
+    for (let i = 0; i < score; i++) {
+        scoreHTML += symbol;
+    }
+    for (let i = score; i < maxScore; i++) {
+        scoreHTML += '⚪';
+    }
+    return scoreHTML;
+}
+
 
 function checkEndGame() {
     if (player1Score >= maxScore || player2Score >= maxScore) {
@@ -388,13 +410,6 @@ function endGame() {
 
     const winner = player1Score >= maxScore ? 'Player 1' : 'Player 2';
     const endGameMessage = document.createElement('div');
-    // endGameMessage.id = 'end-game-message';
-    // endGameMessage.style.position = 'absolute';
-    // endGameMessage.style.top = '50%';
-    // endGameMessage.style.left = '50%';
-		// endGameMessage.style.transform = 'translate(-50%, -50%)';	
-    // endGameMessage.style.fontSize = '2em';
-    // endGameMessage.style.color = '#fff';
     endGameMessage.innerHTML = `${winner} wins!<br>`;
     document.getElementById('gameCont').appendChild(endGameMessage);
 
@@ -412,7 +427,6 @@ function endGame() {
         document.body.removeChild(endGameMessage);
         endGameButtons.style.display = 'none';
         showAllButtons();
-        // hideLayer2Btns();
         resetGame();
         controls.enabled = false;
     });
