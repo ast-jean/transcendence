94567c113755b44567e1ee3319e3d8e18457dd5d
xchouina <xavier@chouinard.email>
2024-09-12 11:07:15 -0400

    virgin branch

--- django_trans/gameserv/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from .consumers import GameConsumer
-from .consumers_truck import GameConsumer_truck
-
-websocket_urlpatterns = [
-    re_path(r'^ws/pong/$', GameConsumer.as_asgi()),
-    re_path(r'^ws/truck/$', GameConsumer_truck.as_asgi()),
-]
