707a27e4116eeca56da7460df45c31d92e6d6335
ast-jean <adam.stjean.lejeune@gmail.com>
2024-10-14 14:42:14 -0400

    remove print in disconnect creating error

--- django_trans/gameserv/consumers.py
+++ django_trans/gameserv/consumers.py
@@ -131,7 +131,6 @@ class GameConsumer(AsyncWebsocketConsumer):
 
                 # Optionally, remove the room if it becomes empty after removing the client
                 if len(room.clients) == 0:
-                    print(f"Room {room['roomId']} is now empty and will be removed.")
                     self.rooms.remove(room)
         
 
@@ -623,7 +622,6 @@ class GameConsumer(AsyncWebsocketConsumer):
 
     async def broadcast_existingPlayers(self, data):
         #Send to every players in room
-        print(data)
         room_id = data.get('data', {}).get('roomId')
         room = await self.find_room(room_id)
         if room is not None:
