e68dbce34bcfbf17369ae8edbdd36fd474172b73
xchouina <xavier@chouinard.email>
2024-10-16 12:50:32 -0400

    changes to settings

--- django_trans/django_trans/settings.py
+++ django_trans/django_trans/settings.py
@@ -159,5 +159,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
